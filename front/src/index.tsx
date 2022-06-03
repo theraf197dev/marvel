389210c15c247a1f76375e58d8179ee836e90b30
theraf197dev <rafael.pla.cab@gmail.com>
2022-06-03 19:00:30 +0200

    fix render

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,15 +1,10 @@
-import { hydrate, render } from "react-dom";
+import { render } from "react-dom";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = document.getElementById("root") as HTMLElement;
 
-if (root.hasChildNodes()) {
-  hydrate(<App />, root);
-} else {
-  render(<App />, root);
-}
-
+render(<App />, root);
 
 reportWebVitals();
